23cd66120cf4e88be1eb5ddfe3a91efa8e880f0c
AYAZ KHAN <ayazbadsha786@gmail.com>
2025-06-13 18:44:41 +0500

    Add some fix on BE also did some change in FE

--- myapp/models.py
+++ myapp/models.py
@@ -45,7 +45,7 @@ class Customer(models.Model):
     phone_number2 = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.name if self.name else "Unnamed Customer"
 
